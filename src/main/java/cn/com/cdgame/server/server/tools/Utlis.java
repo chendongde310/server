b0bfbc9e041465f24df9d9720773117169ed2596
chen <1279262533@qq.com>
2018-01-23 12:08:44 +0800

    添加code功能

--- src/main/java/cn/com/cdgame/server/server/tools/Utlis.java
+++ src/main/java/cn/com/cdgame/server/server/tools/Utlis.java
@@ -38,13 +38,42 @@ public class Utlis {
         }
     }
 
-
+    private static String getType(int code) {
+        switch (code) {
+            case 1:
+                return "头盔";
+            case 2:
+                return "护肩";
+            case 3:
+                return "项链";
+            case 4:
+                return "胸甲";
+            case 5:
+                return "腰带";
+            case 6:
+                return "裤子";
+            case 7:
+                return "护腿";
+            case 8:
+                return "鞋子";
+            case 9:
+                return "手套";
+            case 10:
+                return "戒指";
+            case 11:
+                return "主武器";
+            case 12:
+                return "副武器";
+            default:
+                return "未知";
+        }
+    }
 
     //创建物品数据，传入装备ID,可破坏物ID，装备名字，装备介绍，装备类型，装备品质，装备等级
     // 随机属性数量，必带属性1，必带属性2，必带属性3，必带属性4，必带属性5，必带属性6
     public static String code(Item i) {
 
-        StringBuffer itemcode = new StringBuffer("\n\n//" + i.getName() + "(" + i.getType() + ")").append("\n");
+        StringBuffer itemcode = new StringBuffer("\n\n//" + i.getName() + "(" + getType(i.getType()) + ")").append("\n");
         itemcode.append("//-----------------------------------------\n");
         itemcode.append("call YDLocal1Set(string, \"itemId\", \"装备ID\")").append("\n");
         itemcode.append("call CreateItemDatas(YDLocal1Get(string, \"itemId\"),\"图标ID\",")
