bf989d915683cd40ed0e8544ee4f0748adc39e7b
chen <1279262533@qq.com>
2018-02-13 16:26:56 +0800

    update

--- src/main/java/cn/com/cdgame/server/server/pojo/ItemSP.java
+++ src/main/java/cn/com/cdgame/server/server/pojo/ItemSP.java
@@ -1,44 +1,116 @@
 package cn.com.cdgame.server.server.pojo;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * 饰品
  */
 @Entity
 @Table(name = "t_item_sp")
-public class ItemSP extends Item {
+public class ItemSP {
+    public static final int RANDOM = 666;
+    public static final int NO = 0;
     @Column(name = "item_with")
     private int with; //饰品偏向
-
-
     @Column(name = "item_base1")
     private int base1; //基础伤害
-
     @Column(name = "item_base2")
     private int base2; //体能／魔能
-
     @Column(name = "item_base3")
     private int base3; //基础属性
-
     @Column(name = "item_sx0")
     private int sx0; //防御属性
-
     @Column(name = "item_sx1")
     private int sx1; //攻击属性
-
     @Column(name = "item_sx12")
     private int sx12; //额外攻击属性
-
     @Column(name = "item_sx2")
     private int sx2; //生命／魔法值属性
-
-
     @Column(name = "item_sx3")
     private int sx3; //其他属性
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "item_name")
+    private String name; //物品名称
+    @Column(name = "item_type")
+    private int type; //物品的类型
+    @Column(name = "item_level")
+    private int level;//物品等级
+    @Column(name = "item_rank")
+    private int rank;//装备品质
+    @Column(name = "item_depict")
+    private String depict; //物品描述
+    @Column(name = "item_skill")
+    private String skill; //装备被动技能
+    @Column(name = "item_author")
+    private String author; //装备作者
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public String getDepict() {
+        return depict;
+    }
+
+    public void setDepict(String depict) {
+        this.depict = depict;
+    }
+
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
 
     public int getWith() {
         return with;
--- src/main/java/cn/com/cdgame/server/server/pojo/ItemWQ.java
+++ src/main/java/cn/com/cdgame/server/server/pojo/ItemWQ.java
@@ -1,44 +1,117 @@
 package cn.com.cdgame.server.server.pojo;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * 武器
  */
 @Entity
 @Table(name = "t_item_wq")
-public class ItemWQ extends Item {
+public class ItemWQ {
 
+    public static final int RANDOM = 666;
+    public static final int NO = 0;
     @Column(name = "item_with")
     private int with; //武器偏向
-
     @Column(name = "item_base1")
     private int base1; //护甲
-
     @Column(name = "item_base2")
     private int base2; //攻击速度
-
     @Column(name = "item_base3")
     private int base3; //基础属性
-
     @Column(name = "item_sx0")
     private int sx0; //特殊伤害
-
     @Column(name = "item_sx1")
     private int sx1; //主要攻击属性
-
     @Column(name = "item_sx12")
     private int sx12; //主要攻击属性2
-
-
     @Column(name = "item_sx2")
     private int sx2; //次要随机属性
-
     @Column(name = "item_sx22")
     private int sx22; //次要随机属性2
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "item_name")
+    private String name; //物品名称
+    @Column(name = "item_type")
+    private int type; //物品的类型
+    @Column(name = "item_level")
+    private int level;//物品等级
+    @Column(name = "item_rank")
+    private int rank;//装备品质
+    @Column(name = "item_depict")
+    private String depict; //物品描述
+    @Column(name = "item_skill")
+    private String skill; //装备被动技能
+    @Column(name = "item_author")
+    private String author; //装备作者
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public String getDepict() {
+        return depict;
+    }
+
+    public void setDepict(String depict) {
+        this.depict = depict;
+    }
+
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
 
     public int getWith() {
         return with;
