3e9410ba258ce50b09f65f5ded1cc6f0bb1f8a0c
chen <1279262533@qq.com>
2018-03-27 16:28:08 +0800

    update

--- src/main/java/cn/com/cdgame/server/server/service/OtakuService.java
+++ src/main/java/cn/com/cdgame/server/server/service/OtakuService.java
@@ -1,8 +1,11 @@
 package cn.com.cdgame.server.server.service;
 
+import cn.com.cdgame.server.server.pojo.ItemWQ;
 import cn.com.cdgame.server.server.pojo.Otaku;
 import cn.com.cdgame.server.server.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * 作者：陈东  —  www.renwey.com
@@ -11,9 +14,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface OtakuService extends JpaRepository<Otaku, Long> {
 
-    Otaku findByKey(String key);
 
 
+    List<Otaku> findAllBy();
 
 
 }
